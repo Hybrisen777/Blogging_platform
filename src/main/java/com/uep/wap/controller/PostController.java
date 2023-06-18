3fe0d36e3286f09726b9eaa3fee43b20d42b3aec
jan <83819@student.ue.poznan.pl>
2023-06-18 22:41:19 +0200

    podstawowa obsługa tworzenia postów

--- src/main/java/com/uep/wap/controller/PostController.java
+++ src/main/java/com/uep/wap/controller/PostController.java
@@ -1,12 +1,16 @@
 package com.uep.wap.controller;
 
+import com.uep.wap.model.Account;
 import com.uep.wap.model.Post;
+import com.uep.wap.service.AccountService;
 import com.uep.wap.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Optional;
 
@@ -14,6 +18,8 @@ import java.util.Optional;
 public class PostController {
     @Autowired
     private PostService postService;
+    @Autowired
+    private AccountService accountService;
 
     @GetMapping("/posts/{id}")
     public String getPost(@PathVariable Long id, Model model) {
@@ -23,5 +29,25 @@ public class PostController {
         return "post";
     }
 
+    @GetMapping("/post/create")
+    public String createNewPost(Model model) {
+        //konto user podpięte jako obecny autor wszystkich tworzonych postów
+        Optional<Account> optionalAccount = accountService.findByUsername("user");
+        if (optionalAccount.isPresent()) {
+            Post post = new Post();
+            post.setAccount(optionalAccount.get());
+            model.addAttribute("post",post);
+            return "post_create";
+        } else {
+            //do zrobienia- strona obsługująca błędy
+            return "error";
+        }
+    }
+
+    @PostMapping("/post/create")
+    public String saveNewPost(@ModelAttribute Post post) {
+        postService.save(post);
+        return "redirect:/posts/" + post.getId();
+    }
 
 }
--- src/main/java/com/uep/wap/service/PostService.java
+++ src/main/java/com/uep/wap/service/PostService.java
@@ -26,18 +26,11 @@ public class PostService {
         return postRepository.save(post);
     }
 
-    /**
-    public Post updatePost(Long id, Post post) {
-        Post existingPost = postRepository.findById(id).orElse(null);
-        if (existingPost != null) {
-            existingPost.setTitle(post.getTitle());
-            existingPost.setContent(post.getContent());
-            return postRepository.save(existingPost);
+    public Post save(Post post) {
+        if (post.getId() == null) {
+            post.setCreatedAt(LocalDateTime.now());
         }
-        return null;
-    }
-    **/
-    public void deletePost(Long id) {
-        postRepository.deleteById(id);
+        return postRepository.save(post);
     }
 }
+
